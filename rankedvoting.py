a1b016a97ea1600a66b92ed614666c19768e194b
GnozL <gggonzalez000@gmail.com>
2026-02-11 14:00:35 -0800

    1.0 - COMPLETE: Approval, Point Value, & Plurality

--- rankedvoting.py
+++ rankedvoting.py
@@ -1,109 +1,123 @@
-import csv
-
 class Ballot:
-    def __init__(self, ID, votes, alive = True):  
+    def __init__(self, ID, votes, dead = False):  
 
 	    self.ID = ID
 	    self.votes = votes
-	    self.first = votes[0]
-	    self.second = votes[1]
-	    self.third = votes[2]
-	    self.alive = alive
+	    self.dead = dead
 
 
-def import_csv():
-	ballot = {}
+def import_csv(filename="votes.csv"):
+	import csv
 
-	with open('064clean.csv', newline='') as csvfile:
+	ballots = {}
+
+	with open(filename, newline='') as csvfile:
 		ballotReader = csv.DictReader(csvfile)
 
 		for i in ballotReader:
-			if i['VoterID'] in dq_list: continue
+			if i['VoterID'] in dq_list: 
+				continue
 			voterID = i['VoterID']
 			votes = [i['1st Place'], i['2nd Place'], i['3rd Place'] ]
-			ballot[voterID] = Ballot(voterID, votes)
+			#TODO: Allow every candidate to be ranked, not just top 3 
+			ballots[voterID] = Ballot(voterID, votes)
 
-	return ballot
+	return ballots
 
 
-def disqualified():
+def disqualified(filename="dq.txt"):
+
 	dq_list = []
 
-	with open("dq.txt") as file:
+	if (input("Import DQ list? ") not in ['y', 'Y', 'yes', 'Yes', 'YES']):
+		return dq_list
+
+	with open(filename) as file:
 		dq_list = file.read().splitlines()
+
+	for dq in dq_list:
+		print(dq + " has been disqualified.")
 
 	return dq_list
 
 
-def score_voting(first, second, third):
+def get_candidates():
+	candidates = []
+	for voterID in ballot:
+		for vote in ballot[voterID].votes:
+			if vote not in candidates:
+				candidates.append(vote)
+
+	return candidates
+
+
+def score_voting(points):
+
 	tally = {}
+
 	for ID in ballot:
 
-		for index, vote in ballot[ID].votes:
-			print(index)
+		if ballot[ID].dead:
+			continue
+
+		for index, vote in enumerate(ballot[ID].votes):
+			if vote in dq_list:
+				continue
 			if vote not in tally:
 				tally[vote] = 0
-			tally[vote] += 3-index
+			if 0 <= index < len(points):
+				tally[vote] += points[index]
 
-"""		if ballot[ID].first not in tally:
-			tally[ballot[ID].first] = 0
-		if ballot[ID].second not in tally:
-			tally[ballot[ID].second]= 0
-		if ballot[ID].third not in tally:
-			tally[ballot[ID].third] = 0
-"""
-		tally[ballot[ID].first]	+= first
-		tally[ballot[ID].second]+= second
-		tally[ballot[ID].third]	+= third
 
-		for dq in dq_list:
-			if dq in tally:
-				del tally[dq]
-
-	winner = max(tally, key=tally.get)
-	print("Winner is " + winner)
 
 	return tally
 
 
 def instant_runoff():
-	runoff = copy.deepcopy(ballot)
-	candidates = []
-	for vote in runoff
-		if runoff[vote].first not in candidates:
-			candidates.append(runoff[vote].first)
-		if runoff[vote].second not in candidates:
-			candidates.append(runoff[vote].second)
-		if runoff[vote].third not in candidates:
-			candidates.append(runoff[vote].third)
+	import copy
+	new_ballot = copy.deepcopy(ballot)
+	new_candidates = copy.deepcopy(candidates)
+
+	#TODO: Finish this
 
 
 def tally_votes(mode):
 
-	if mode == "1":
+	if mode == "1" or "0":
 		mode = "Point"
-		tally = score_voting(3,2,1)	
+		tally = score_voting([3,2,1])
+		winner = max(tally, key=tally.get)
+		print("Point Value Winner is " + winner + " with " + str(tally[winner]) + " out of " + str(3*voters) + " maximum possible points.")
 
-	elif mode == "2": 
+
+	if mode == "2" or "0": 
 		mode = "Approval"
-		tally = score_voting(1,1,1)
+		tally = score_voting([1,1,1])
+		winner = max(tally, key=tally.get)
+		print(str(tally[winner]) + " out of " + str(voters) + " voters approve of " + winner + " as the winner.")
 
-	elif mode == "3": 
+
+	if mode == "3" or "0": 
 		mode = "Plurality"
-		tally = score_voting(1,0,0)
+		tally = score_voting([1])
+		winner = max(tally, key=tally.get)
+		print(winner + " won, with " + str(tally[winner]) + " first place votes.")
 
-	elif mode == "4":
+
+	elif mode == "4" or "0":
 		mode = "Instant Runoff"
 		tally = instant_runoff()
+
+	#TODO: 
 
 	else: return Exception("tally_votes argument error")
 
 
 def run():
 	print("Enter tallying method: ")
-	print("1. Approval\n2. Point Value\n3. First Past the Post\n4. Instant Runoff")
+	print("1. Point Value\n2. Approval\n3. First Past the Post\n4. Instant Runoff\n0. Run all the simulations.")
 	mode = input()
-	options = ["1", "2", "3","4"]
+	options = ["0", "1", "2", "3","4"]
 	if mode not in options:
 		return print("Please select an available option.")
 	tally_votes(mode)
@@ -111,6 +125,8 @@ def run():
 
 dq_list = disqualified()
 ballot = import_csv()
+candidates = get_candidates()
+voters = len(ballot)
 run()
 
 while(input("Run again? (Y/N): ") in ['y', 'Y', 'yes', 'Yes', 'YES']):
